4b70dccb4240318c1d1ac3d80f3d53d624460c50
malec0101 <malec0101@gmail.com>
2024-02-09 16:38:28 +0300

    fix path site & add vol static in static.sh

--- 07-docker/static.sh
+++ 07-docker/static.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-docker run -d -it --rm --name hometask-static -v $PWD:/mnt -p 8081:80 hometask-image:v1 /bin/sh &&
-cat <<EOF | docker exec -i hometask-static sh 
-rm /var/www/dynamic/index.php
-cp /mnt/index.html /var/www/dynamic/index.html
-/usr/sbin/apache2ctl restart
-EOF
+docker run -d --rm --name hometask-static \
+  -v $PWD/files/static:/var/www/html/ \
+  -p 8081:80 hometask-image:v1
