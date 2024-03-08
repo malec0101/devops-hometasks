a0bcd00d0a272102c013cfa2bd4052c6bcead2e4
Eugene Malechko <79465442+malec0101@users.noreply.github.com>
2024-03-08 11:09:15 +0300

    Managed by Terraform

---
+++ 13-terraform/vars.tf
@@ -0,0 +1,36 @@
+variable "names_res_kub" {
+  
+  type = object({
+    service    = string
+    ingress    = string
+    deployment = string
+  })
+}
+
+variable "labels" {
+  type         = string
+  default      = "word-cloud-generator"
+}
+
+variable "spec_container" {
+  type = object({
+    name        = string
+    image       = string
+    port        = number
+    target_port = number
+    protocol    = string
+  })
+}
+
+variable "spec_ingress" {
+  type = object({
+    host        = string
+    path        = string
+    path_type   = string
+  })
+}
+
+variable "github_owner" {
+  type = string
+}
+variable "tf_token" {}
