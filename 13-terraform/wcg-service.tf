8fd5200936101f082446c8b8d7035e13ebfa3771
Eugene Malechko <79465442+malec0101@users.noreply.github.com>
2024-03-08 11:09:05 +0300

    Managed by Terraform

---
+++ 13-terraform/wcg-service.tf
@@ -0,0 +1,21 @@
+resource "kubernetes_service" "word_cloud_generator_service" {
+  metadata {
+    name = var.names_res_kub.service
+
+    labels = {
+      app = var.labels
+    }
+  }
+
+  spec {
+    port {
+      protocol    = var.spec_container.protocol
+      port        = var.spec_container.port
+      target_port = var.spec_container.target_port
+    }
+
+    selector = {
+      app = var.labels
+    }
+  }
+}
